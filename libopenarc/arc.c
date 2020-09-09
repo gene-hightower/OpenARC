07b5be743d1e36094096a26cf99144ebfc223ffc
Gene Hightower <gene@digilicious.com>
2020-09-09 15:27:45 -0700

    Use CRLF line endings, not native Unix style LF only

--- libopenarc/arc.c
+++ libopenarc/arc.c
@@ -641,7 +641,7 @@ arc_getamshdr_d(ARC_MESSAGE *msg, size_t initial, u_char **buf, size_t *buflen,
 			{
 				forcewrap = FALSE;
 				arc_dstring_catn(msg->arc_hdrbuf,
-				                  (u_char *) "\n\t", 2);
+				                  (u_char *) "\r\n\t", 3);
 				len = 8;
 
 				if (strcmp(which, "h") == 0)
@@ -671,8 +671,8 @@ arc_getamshdr_d(ARC_MESSAGE *msg, size_t initial, u_char **buf, size_t *buflen,
 							                 ':');
 							len += 1;
 							arc_dstring_catn(msg->arc_hdrbuf,
-							                 (u_char *) "\n\t ",
-							                 3);
+							                 (u_char *) "\r\n\t ",
+							                 4);
 							len = 9;
 							arc_dstring_catn(msg->arc_hdrbuf,
 							                 (u_char *) tmp,
@@ -719,8 +719,8 @@ arc_getamshdr_d(ARC_MESSAGE *msg, size_t initial, u_char **buf, size_t *buflen,
 						if (msg->arc_margin - len == 0)
 						{
 							arc_dstring_catn(msg->arc_hdrbuf,
-							                  (u_char *) "\n\t ",
-							                  3);
+							                  (u_char *) "\r\n\t ",
+							                  4);
 							len = 9;
 						}
 
